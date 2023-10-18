882ee79da41669187a91b0426d5757dfa6ac98c2
DEVnitishOfficial <nitish.naroun123@gmail.com>
2023-10-18 20:41:22 +0530

    used reduxToolkit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-2xl font-bold text-green-600'>TODOS OF REDUX_TOOLKIT</h1>
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
